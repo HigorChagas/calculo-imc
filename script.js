dbd340fae5badba5665924c822bbe8b168b30d0d
Higor Chagas <higornchagas@gmail.com>
2021-05-26 19:48:15 -0300

    criando o JS da calculadora

---
+++ script.js
@@ -0,0 +1,15 @@
+function calcular() {
+    var nome = window.document.getElementById('nome')
+    var altura = window.document.getElementById('altura')
+    var peso = window.document.getElementById('peso')
+
+    var resultado = window.document.getElementById('resultado')
+
+    var nome = String(nome.value)
+    var numeroAltura = Number(altura.value)
+    var numeroPeso = Number(peso.value)
+
+    var calculoImc = numeroPeso / (numeroAltura * numeroAltura)
+
+    resultado.innerHTML = `Olá ${nome}, o seu IMC é: ${calculoImc.toFixed(2)}`
+}
