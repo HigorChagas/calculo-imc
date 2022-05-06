1a6623592652897a40d2dddc8ab68fe5a7c0826e
Higor Chagas <higornchagas@gmail.com>
2022-05-06 09:44:09 -0300

    Testes de desáfios do CodeWars

--- teste.js
+++ teste.js
@@ -49,10 +49,120 @@
 
 // validatePIN("12a34")
 
-const solution = (str, ending) => {
-  console.log(str.endsWith(ending))
+// const solution = (str, ending) => {
+//   console.log(str.endsWith(ending))
   
+// }
+
+// solution('abcde', 'cde');
+// solution('abcde', 'abc')
+
+// const isSquare = (n) => {
+//   if(n < 0) console.log(false)
+
+//   const squareRoot = Math.floor(Math.sqrt(n));
+//   const squareNumber = (squareRoot * squareRoot);
+//   console.log(Math.floor(squareNumber), n, squareRoot);
+
+//   console.log(squareNumber === n ? true : false);
+
+
+// }
+
+// isSquare(27);
+
+// const findNeedle = (haystack) => {
+//   const needleIndex = haystack.findIndex(haystack => haystack === 'needle');
+  
+//   console.log()
+
+// }
+
+// findNeedle([1,2,3,4,5,6,7,8,8,7,5,4,3,4,5,6,67,5,5,3,3,4,2,34,234,23,4,234,324,324,'needle',1,2,3,4,5,5,6,5,4,32,3,45,54]);
+
+// const greet = (language) => {
+//   const database = {
+//     english: 'Welcome',
+//     czech: 'Vitejte',
+//     danish: 'Velkomst',
+//     dutch: 'Welkom',
+//     estonian: 'Tere tulemast',
+//     finnish: 'Tervetuloa',
+//     flemish: 'Welgekomen',
+//     french: 'Bienvenue',
+//     german: 'Willkommen',
+//     irish: 'Failte',
+//     italian: 'Benvenuto',
+//     latvian: 'Gaidits',
+//     lithuanian: 'Laukiamas',
+//     polish: 'Witamy',
+//     spanish: 'Bienvenido',
+//     swedish: 'Valkommen',
+//     welsh: 'Croeso'
+//   }
+  
+//   !database[language] ? console.log('testando') : console.log(database[language]);
+  
+// }
+
+// greet('spanish');
+
+// const binaryArrayToNumber = arr => {
+//     let dec = 0;
+//     for(let i = 0;i < arr.length; i++) {
+//         dec += Math.pow(2, i) * arr[arr.length - i - 1];
+//     }
+
+//     console.log(dec);
+// };
+
+// binaryArrayToNumber([0,0,0,1])
+
+// function isPalindrome(x) {
+//     const reverseString = x.split('').reverse().join('').toLowerCase();
+    
+//     console.log(x.toLowerCase() === reverseString ? true : false);
+    
+// }
+
+// isPalindrome("Texto");
+
+// function howMuchILoveYou(nbPetals) {
+//     let = count = '';
+//     const love = [
+//         'I love you',
+//         'a little',
+//         'a lot',
+//         'passionately',
+//         'madly',
+//         'not at all'
+//     ];
+
+//     for(let i = 0; i < love.length; i++) {
+//         console.log(love[7]);
+//     }
+// }
+
+// howMuchILoveYou(3)
+
+const arithmetic = (a, b, operator) => {
+
+    switch(operator) {
+        case 'add':
+            console.log(a + b);
+            break;
+        case 'subtract':
+            console.log(a - b);
+            break;
+        case 'multiply':
+            console.log(a * b);
+            break;
+        case 'divide':
+            console.log(a / b);
+            break;
+        default:
+            console.log('ERROR');
+    }
 }
 
-solution('abcde', 'cde');
-solution('abcde', 'abc')
+arithmetic(5, 2, 'subtract');
